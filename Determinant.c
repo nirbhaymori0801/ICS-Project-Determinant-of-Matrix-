e2c59b7114c1e7004548965af5719f58a558403e
nirbhaymori0801 <nirbhay.mori.0801@gmail.com>
2025-11-04 15:23:35 +0530

    Update Determinant.c

--- Determinant.c
+++ Determinant.c
@@ -1,1 +1,11 @@
 #include<stdio.h>
+int main(){
+  int mat[10000][10000],n;
+  printf("Write the number of rows/columns of square matrix:");
+  scanf("%d",&n);
+  printf("Write the elements of matrix:\n);
+  for(int i=0;i<n;i++)
+    for(int j=0;j<n;j++)
+        scanf("%d",&mat[i][j]);
+  
+}
